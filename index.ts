ce75a775b3e629563afd1bee6742835a3ff8ac82
Matias Frith <frithmatias@gmail.com>
2020-07-08 08:25:13 -0300

    routing

--- index.ts
+++ index.ts
@@ -1,6 +1,3 @@
-// const nombre = "Matias";
-// console.log(`Mi nombre es ${ nombre }`);
-
 //import { SERVER_PORT } from "./global/environment";
 import Server from './classes/server';
 import router from './routes/router';
@@ -9,25 +6,18 @@ import cors from 'cors';
 import path from 'path';
 import express from 'express';
 
-// si en router.ts lo explorto de la siguiente manera
-// const router = Router();
-// export default router;
-// puedo usar la importacion sin llaves
-// import router from "./routes/router";
-
-// si en cambio lo exporto de la siguiente manera
-// export const router = Router();
-// tengo que usar la importacion con llaves
-// import { router } from "./routes/router";
-
-// Patron SINGLETON, es una configuración adicional a mi clase Server para asegurarme de tener
-// una UNICA instancia del servidor de socktes como de todas las propiedades de mi clase Server.
 // const server = new Server();
-const server = Server.instance; // obtenemos una nueva instancia de forma estática
+const server = Server.instance; // singleton
 
+// public
 const publicPath = path.resolve(__dirname, '../public');
-console.log(__dirname);
 server.app.use(express.static(publicPath));
+
+// evitar problemas de rutas 
+const indexPath = path.resolve(__dirname, '../public', 'index.html');
+server.app.get("*", (req, res) => {
+	res.sendFile(indexPath);
+});
 
 // Lo que reciba por el body, lo toma y lo convierte en un objeto de JavaScript
 server.app.use(bodyParser.urlencoded({ extended: true }));
