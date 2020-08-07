53a42709b7e9e86d6ae41e60e7eca6ad3bd7485b
Matias Frith <frithmatias@gmail.com>
2020-08-06 21:46:42 -0300

    fix service worker catch promises on create caches

--- public/sw.js
+++ public/sw.js
@@ -15,7 +15,6 @@ self.addEventListener('install', e => {
       '/app.js',
       '/favicon.ico',
       '/index.html',
-      '/offline.html',
       '/manifest.json',
       '/4.affdce382e8924eb57cf.js',
       '/5.ed30718bd3e8060312ab.js',
@@ -26,15 +25,19 @@ self.addEventListener('install', e => {
       '/runtime.07aa485f3aa7d4644c9e.js',
       '/styles.47142339f0ef8f29ec91.css'
     ])
+  }).catch(()=>{
+    console.log('error al crear la app shell');
   })
 
   let inmutable = caches.open(CACHE_INMUTABLE).then(cache => {
     return cache.addAll([
       'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'
     ])
+  }).catch(()=>{
+    console.log('error al crear el cache inmutable');
   })
 
-  waitUntil(Promise.all([inmutable, static]))
+  e.waitUntil(Promise.all([inmutable, static]))
 })
 
 // ----------------------------------------------------------
@@ -56,86 +59,132 @@ self.addEventListener('activate', e => {
 // ----------------------------------------------------------
 
 self.addEventListener('fetch', e => {
-  const respuesta = caches
-    .match(e.request)
-    .then(resp => {
-      if (resp) {
-        return resp
-      }
-      return fetch(e.request).then(resp => {
-        if (e.request.method !== 'POST') {
-          caches.open(CACHE_DYNAMIC).then(cache => {
-            cache.put(e.request, resp.clone())
-          })
+  if (e.request.url.includes('webturnos.herokuapp.com')) {
+    console.log('fetch:', e.request.url)
+    // las peticiones GET no debe guardarlas en cache
+    return fetch(e.request).then(resp => {
+      return resp
+    })
+  } else {
+    console.log('find cache:', e.request.url)
+    const respuesta = caches
+      .match(e.request)
+      .then(resp => {
+        if (resp) {
+          return resp
         }
-        return resp.clone()
+        return fetch(e.request).then(resp => {
+          if (e.request.method !== 'POST') {
+            caches.open(CACHE_DYNAMIC).then(cache => {
+              cache.put(e.request, resp.clone())
+            })
+          }
+          return resp.clone()
+        })
       })
-    })
-    .catch(err => {
-      if (e.request.headers.get('accept').includes('text/html')) {
-        return caches.match('/offline.html')
-      }
-    })
+      .catch(err => {
+        if (e.request.headers.get('accept').includes('text/html')) {
+          return caches.match('/offline.html')
+        }
+      })
 
-  e.respondWith(respuesta)
+    e.respondWith(respuesta)
+  }
 })
-
 
 // escuchar push
 self.addEventListener('push', e => {
-  const data = JSON.parse(e.data.text());
-  const title = data.title;
-  const msg = data.msg;
-  
+  const data = JSON.parse(e.data.text())
+  const title = data.title
+  const msg = data.msg
+
   const options = {
     body: msg,
-    vibrate: [0,300,100,50,100,50,100,50,100,50,100,50,100,50,150,150,150,450,100,50,100,50,150,150,150,450,100,50,100,50,150,150,150,450,150,150],
+    vibrate: [
+      0,
+      300,
+      100,
+      50,
+      100,
+      50,
+      100,
+      50,
+      100,
+      50,
+      100,
+      50,
+      100,
+      50,
+      150,
+      150,
+      150,
+      450,
+      100,
+      50,
+      100,
+      50,
+      150,
+      150,
+      150,
+      450,
+      100,
+      50,
+      100,
+      50,
+      150,
+      150,
+      150,
+      450,
+      150,
+      150
+    ],
     icon: 'assets/img/icons/icon-72x72.png',
     badge: 'img/favicon.ico',
     openUrl: 'https://webturnos.herokuapp.com',
     data: {
       url: 'https://webturnos.herokuapp.com'
     },
-    actions: [ // solo permite dos acciones válidas se muestran como BOTONES en la notificación.
+    actions: [
+      // solo permite dos acciones válidas se muestran como BOTONES en la notificación.
       {
-          action: 'ver-pantalla',
-          title: 'Ver Pantalla',
-          // icon: 'assets/avatars/thor.jpg'
+        action: 'ver-pantalla',
+        title: 'Ver Pantalla'
+        // icon: 'assets/avatars/thor.jpg'
       },
       {
-          action: 'obtener-turno',
-          title: 'Obtener Turno',
-         // icon: 'assets/avatars/ironman.jpg'
+        action: 'obtener-turno',
+        title: 'Obtener Turno'
+        // icon: 'assets/avatars/ironman.jpg'
       }
-  ]
-  };
-  // como toda accion en el SW tengo que esperar a que termine de realizar toda la notificación 
+    ]
+  }
+  // como toda accion en el SW tengo que esperar a que termine de realizar toda la notificación
   // porque puede demorar unos segundos.
-  e.waitUntil( self.registration.showNotification(title, options));
+  e.waitUntil(self.registration.showNotification(title, options))
 })
 
 self.addEventListener('notificationclick', e => {
-  const notificacion = e.notification;
-  const accion = e.action;
-  notificacion.close();
+  const notificacion = e.notification
+  const accion = e.action
+  notificacion.close()
 })
 
 self.addEventListener('notificationclick', e => {
-  const notificacion = e.notification;
-  const accion = e.action;
-  //matchAll() busca en todas las pestañas abiertas del mismo sitio, y regresa una promesa 
-  const respuesta = clients.matchAll().then( clientes => {
-      // clientes es un array de todos los tabs abiertos de mi aplicación yo sólo quiero el que se encuentra visible 
-      let cliente = clientes.find( c => {
-          return c.visibilityState === 'visible';
-      })
-      if ( cliente !== undefined ) {
-          cliente.navigate( notificacion.data.url );
-          cliente.focus();
-      } else {
-          clients.openWindow( notificacion.data.url ); // me abre una nueva pestaña pero no es lo que yo quiero 
-      }
-      return notificacion.close();
-  }) 
-  e.waitUntil( respuesta ); 
+  const notificacion = e.notification
+  const accion = e.action
+  //matchAll() busca en todas las pestañas abiertas del mismo sitio, y regresa una promesa
+  const respuesta = clients.matchAll().then(clientes => {
+    // clientes es un array de todos los tabs abiertos de mi aplicación yo sólo quiero el que se encuentra visible
+    let cliente = clientes.find(c => {
+      return c.visibilityState === 'visible'
+    })
+    if (cliente !== undefined) {
+      cliente.navigate(notificacion.data.url)
+      cliente.focus()
+    } else {
+      clients.openWindow(notificacion.data.url) // me abre una nueva pestaña pero no es lo que yo quiero
+    }
+    return notificacion.close()
+  })
+  e.waitUntil(respuesta)
 })
